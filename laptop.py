782ff9f616177df6141f4efd52debb1188f51b51
subashdhanush <85820233+subashdhanush@users.noreply.github.com>
2024-08-06 18:02:57 +0530

    Add files via upload

---
+++ laptop.py
@@ -0,0 +1,25 @@
+class laptop():
+    price="0"
+    proc=""
+    ram=""
+
+hp=laptop()
+dell=laptop()
+
+hp.price=5000
+hp.proc="i5"
+hp.ram="8gb"
+
+dell.price=60000
+dell.proc="i7"
+dell.ram="16gb"
+
+
+print(hp.price)
+print(hp.proc)
+print(hp.ram)
+
+
+print(dell.price)
+print(dell.proc)
+print(dell.ram)
