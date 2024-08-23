8aba450d31a3683b094f281a1cc9646b0b10e905
subashdhanush <85820233+subashdhanush@users.noreply.github.com>
2024-08-23 18:04:05 +0530

    Add files via upload

---
+++ constructorexample.py
@@ -0,0 +1,18 @@
+class student:
+    def __init__(self):
+        self.name="dkkdksd"
+        self.regno="544544"
+    def display(self):
+        print("Name",self.name)    
+        print("Reg No",self.regno) 
+s1=student()
+s2=student()
+s1.name="Manoj"
+s1.regno="45"
+s2.name="Karthi"
+s2.regno="1"
+print(s1.name)   
+print(s1.regno)
+print(s2.name)   
+print(s2.regno)
+# s1.display()      
